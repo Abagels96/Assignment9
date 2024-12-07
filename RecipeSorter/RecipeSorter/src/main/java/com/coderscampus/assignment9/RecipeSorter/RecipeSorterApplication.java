94866466f4c6fe5dd29795847f1315f71e66ff33
Abigail Bales <138115235+Abagels96@users.noreply.github.com>
2024-12-07 14:45:05 -0600

    first commit

---
+++ RecipeSorter/RecipeSorter/src/main/java/com/coderscampus/assignment9/RecipeSorter/RecipeSorterApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.assignment9.RecipeSorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSorterApplication.class, args);
+	}
+
+}
---
+++ RecipeSorter/RecipeSorter/src/test/java/com/coderscampus/assignment9/RecipeSorter/RecipeSorterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.assignment9.RecipeSorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
