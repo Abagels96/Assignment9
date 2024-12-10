1cc4c6b62351bb96ea22a14bd4ce2119a9c1f334
Abigail Bales <138115235+Abagels96@users.noreply.github.com>
2024-12-09 20:45:43 -0600

    uncommenting lines

--- RecipeSorter/RecipeSorter/src/main/java/com/coderscampus/recipesorter/service/RecipeSorterService.java
+++ RecipeSorter/RecipeSorter/src/main/java/com/coderscampus/recipesorter/service/RecipeSorterService.java
@@ -34,56 +34,56 @@ public class RecipeSorterService {
 				                                       .withSkipHeaderRecord()
 				                                       .withEscape('\\')
 				                                       .parse(in);
-		int recordCount=0;
 		for (CSVRecord record : records) {
 		
 		System.out.println("the record the program is processing is:"+record+"the number is "+recordCount);
-		recordCount++;
-//			Integer cookingMinutes= Integer.parseInt(record.get(0));
-//			
-//			
-//			
-//			Boolean dairyFree= Boolean.parseBoolean(record.get(1));
-//			
-//			Boolean glutenFree= Boolean.parseBoolean(record.get(2));
-//			
-//		 String instructions= record.get(3);
+//		System.out.println("the record the program is processing is:"+ record+"the number is "+recordCount);
+			Integer cookingMinutes= Integer.parseInt(record.get(0));
+			
+			
+			
+			Boolean dairyFree= Boolean.parseBoolean(record.get(1));
+			
+			Boolean glutenFree= Boolean.parseBoolean(record.get(2));
+			
+		 String instructions= record.get(3);
+
+		     Double preparationMinutes= Double.parseDouble(record.get(4));
+
+		     Double pricePerServing=Double.parseDouble(record.get(5));
+		     
+
+		 Integer readyInMinutes= Integer.parseInt(record.get(6));
+
+		    Integer servings=Integer.parseInt(record.get(7));
+
+		     Double spoonacularScore= Double.parseDouble(record.get(8));
+
+		    String title=record.get(9);
 //
-//		     Double preparationMinutes= Double.parseDouble(record.get(4));
+		    Boolean vegan=Boolean.parseBoolean(record.get(10));
 //
-//		     Double pricePerServing=Double.parseDouble(record.get(5));
-//		     
-//
-//		 Integer readyInMinutes= Integer.parseInt(record.get(6));
-//
-//		    Integer servings=Integer.parseInt(record.get(7));
-//
-//		     Double spoonacularScore= Double.parseDouble(record.get(8));
-//
-//		    String title=record.get(9);
-////
-//		    Boolean vegan=Boolean.parseBoolean(record.get(10));
-////
-//		    Boolean vegetarian=Boolean.parseBoolean(record.get(11));
-//			
+		    Boolean vegetarian=Boolean.parseBoolean(record.get(11));
+			
 //			i++;
 //			System.out.println(i);
-//			System.out.println("Cooking Minutes are" +cookingMinutes);
-//			System.out.println("Is it dairy free?" + dairyFree);
-//			System.out.println("Is it gluten free" + glutenFree);
-//System.out.println("Here are the instructions"+instructions);			
-//System.out.println("number of minutes"+preparationMinutes);			
-//System.out.println("Price per serving"+pricePerServing);			
-//System.out.println("ready in minutes"+readyInMinutes);			
-//System.out.println("number of servings"+ servings);			
-//System.out.println("spoonacular score"+spoonacularScore);			
-//System.out.println("here is the title"+title);			
-//System.out.println("is it vegan"+vegan);			
-//System.out.println("Is it vegetarian"+vegetarian);			
-//			
-//addRecipe(cookingMinutes, dairyFree, glutenFree,  instructions, preparationMinutes, pricePerServing, readyInMinutes, servings, spoonacularScore,title, vegan,vegetarian);
+			System.out.println("Cooking Minutes are" +cookingMinutes);
+			System.out.println("Is it dairy free?" + dairyFree);
+			System.out.println("Is it gluten free" + glutenFree);
+System.out.println("Here are the instructions"+instructions);			
+System.out.println("number of minutes"+preparationMinutes);			
+System.out.println("Price per serving"+pricePerServing);			
+System.out.println("ready in minutes"+readyInMinutes);			
+System.out.println("number of servings"+ servings);			
+System.out.println("spoonacular score"+spoonacularScore);			
+System.out.println("here is the title"+title);			
+System.out.println("is it vegan"+vegan);			
+System.out.println("Is it vegetarian"+vegetarian);			
+			
+addRecipe(cookingMinutes, dairyFree, glutenFree,  instructions, preparationMinutes, pricePerServing, readyInMinutes, servings, spoonacularScore,title, vegan,vegetarian);
 		
 		}
+//		recordCount++;
 		
 	
 		
